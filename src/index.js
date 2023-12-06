6bab646e584020d7d6d134ea6403901c7846deec
Banessa Mikhaylova <maebanessa@gmail.com>
2023-12-06 15:34:21 +0300

    Auto stash before merge of "master" and "origin/master"
    1

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// Рендеринг приложения с использованием Redux Store - Provider
+
 root.render(
   <Provider store={store}>
     <App />
